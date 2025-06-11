d4311def204ca6639212e5b8e7dfa8a5d21ce1eb
smaurya4941 <smaurya4941@gmail.com>
2025-06-11 15:53:13 +0530

    third commit

--- TokenAuthentication/api/views.py
+++ TokenAuthentication/api/views.py
@@ -8,4 +8,5 @@ from rest_framework.permissions import IsAuthenticated
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
-    
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
