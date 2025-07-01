72cbb0b08e789c7d8ecd66a76ffc318ab354452b
smaurya4941 <smaurya4941@gmail.com>
2025-07-01 15:58:03 +0530

    djangofilters

--- djangoFilter/api/views.py
+++ djangoFilter/api/views.py
@@ -3,24 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Student
 from .serializers import StudentSerializers
+from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework.generics import ListAPIView
-
-# class StudentListView(ListAPIView):
-#     queryset=Student.objects.all()
-#     serializer_class=StudentSerializers
-
-
-# class StudentListView(ListAPIView):
-#     queryset=Student.objects.filter(passby='user2')
-#     serializer_class=StudentSerializers
-
-
-
 class StudentListView(ListAPIView):
     queryset=Student.objects.all()
     serializer_class=StudentSerializers
+    filterset_fields=['city']
 
-    def get_queryset(self):    #filter only those student # who are passed by the current logged in user/teacher user
-        user=self.request.user
-        return Student.objects.filter(passby=user)
+
+#For per view filtering
+class StudentListView(ListAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializers
+    filter_backends=[DjangoFilterBackend]
+    filterset_fields=['city']
